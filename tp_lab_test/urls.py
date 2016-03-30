f88df368b1e88e676e724229caf6657192b30ded
maratkanov-a <a.s.maratkanov@gmail.com>
2016-03-30 13:10:20 +0300

    added icons, also added login/logout

--- tp_lab_test/urls.py
+++ tp_lab_test/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^login/', 'django.contrib.auth.views.login', name='login-page'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout-page'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('todo_list.urls')),
 ]
