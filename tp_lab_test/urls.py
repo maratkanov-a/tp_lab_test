e1c6986c605e049b638d13ca8d2261c44df0f0c3
maratkanov-a <a.s.maratkanov@gmail.com>
2016-03-31 10:23:39 +0300

    fixed login/logout/signin styles

--- tp_lab_test/urls.py
+++ tp_lab_test/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^login/', 'django.contrib.auth.views.login', name='login-page'),
-    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout-page'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('todo_list.urls')),
