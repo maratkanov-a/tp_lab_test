5b0f79d7234608091a3353aac40db0c27007a5bb
maratkanov-a <a.s.maratkanov@gmail.com>
2016-03-31 12:46:32 +0300

    fixed styles for main page

--- todo_list/static/js/mine.js
+++ todo_list/static/js/mine.js
@@ -1,11 +1,3 @@
-//$(document).ready(function() {
-////$('.datepicker').pickadate({
-////    selectMonths: true, // Creates a dropdown to control month
-////    selectYears: 15, // Creates a dropdown of 15 years to control year
-////    format: 'dd-mm-yyyy' });
-////
-//});
-
 $('.js-add-task').on('submit', function(e) {
      e.preventDefault();
     var new_this = $(this);
